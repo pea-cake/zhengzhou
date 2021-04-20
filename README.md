ad9a0d9f1ea14fc9f875322aaf5a4a2dd4547130
xumeng <xmdevr@gmail.com>
2021-04-20 11:55:13 +0800

    feat:郑州房产销售情况

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# zhengzhou
-about zhengzhou
+# 郑州房产销售情况爬虫数据(zhengzhou)
+![avatar](/2016-2021房产销售情况.png)
+## 可根据自己需求增加其他数据字段（二手房销售情况...）
---
+++ index.js
@@ -0,0 +1,114 @@
+const request = require('request')
+const iconv = require('iconv-lite');
+
+// 引入cheerio模块
+const cheerio = require('cheerio')
+
+const fs = require('fs'); // 引入fs模块
+
+totalPage = 1
+
+function readFile(filename){
+    return new Promise((res,rej)=>{
+        // 读取
+        fs.readFile(filename, 'utf-8', function(err, data) {
+            if (err) {
+                rej(err);
+                console.log(err)
+            }
+            const areas = JSON.parse(data)
+            res(areas)
+        });
+    })
+}
+
+function getHouseList(page){
+    return new Promise((res,rej)=>{
+        request.get({encoding: null, url:'http://public.zhengzhou.gov.cn/?a=path&d=19&p=D12Y&page='+page},function (err, response, body) {
+            let html =iconv.decode(body,'utf8'); 
+                // 加载HTML字符串
+            let $ = cheerio.load(html)
+            // 获取完整HTML
+            totalPage = $('.page-tile a').eq(-2).html()
+            let houses = $(".page-list a")
+            let houseshtml = []
+            houses.each((i,item)=>{
+                let title = $(item).find('span').html()
+                let url = $(item).attr('href')
+                houseshtml.push({
+                    title: title,
+                    url:url
+                })
+            })
+            res(houseshtml)       
+        })
+    })
+    
+}
+const hourseNumReg = new RegExp('商品房销售(.*?)套（间）')
+const areasReg = new RegExp('销售面积(.*?)万平方米')
+const avgPriceReg = new RegExp('销售均价(.*?)元\/平方米')
+function getHouseDetail(title_url){
+    // 郑州全市商品房销售10130套（间），销售面积108.37万平方米，销售均价12084元/平方米
+    return new Promise((res,rej)=>{
+        request.get({encoding: null, url:title_url.url},function (err, response, body) {
+            // 写入文件内容（如果文件不存在会创建一个文件）
+            let html =iconv.decode(body,'utf8'); 
+            //     // 加载HTML字符串
+            let $ = cheerio.load(html) 
+            let str = $('.content-wrap').text()
+            let hourseNum = str.match(hourseNumReg)
+            let areas = str.match(areasReg)
+            let avgPrice = str.match(avgPriceReg)
+            // console.log(hourseNum,areas,avgPrice)
+            console.log(title_url.title,hourseNum[1],areas[1],avgPrice[1])
+            let line_data = title_url.title+'   '+hourseNum[1]+'   '+areas[1]+'   '+avgPrice[1]
+            res(line_data)                     
+        })
+    })
+    
+}
+
+function Sleep(num){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            console.log('sleeping'+num+'...')
+            res()
+        },num*1000)
+    })
+}
+
+function getAllHouseList(){
+    return new Promise(async(res,rej)=>{
+        let all_house_list  = []
+        all_house_list = await getHouseList(1)
+        console.log('---->',totalPage)
+        if(totalPage>1){
+            for(let i=2;i<=totalPage;i++){
+                let houselist = await getHouseList(i)
+                all_house_list = all_house_list.concat(houselist)
+            }
+        }
+        console.log(all_house_list.length)
+        res(all_house_list)
+    })          
+}
+
+async function main(){
+    let allHouseList = await getAllHouseList()
+    console.log('2016-2021---郑州市房地产市场销售情况 数量(套) 面积(万平米) 均价(元)')
+    let allDetail = '2016-2021---郑州市房地产市场销售情况 数量(套) 面积(万平米) 均价(元)\r\n'
+    for(let i=0;i<allHouseList.length;i++){
+        let res = await getHouseDetail(allHouseList[i])
+        allDetail +=(res+'\r\n')
+    }
+    fs.writeFile('./郑州市房地产市场销售情况.txt', JSON.stringify(allDetail), { 'flag': 'w+' }, function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('success');
+        console.log(allDetail)
+    });    
+}
+
+main()
